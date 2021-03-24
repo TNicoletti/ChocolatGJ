fc82348b04d30828ab2a7bf732a95fe24b96e889
TNicoletti <30930753+TNicoletti@users.noreply.github.com>
2021-03-23 23:53:31 -0300

    Jogador se move e colide

--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-const MOVESPEED_N = 100
+const MOVESPEED_N = 300
 const MOVESPEED_T = MOVESPEED_N*2/3
 
 var movespeed = MOVESPEED_N
@@ -13,5 +13,17 @@ func _ready():
 func _physics_process(delta):
 	var motion = Vector2()
 	
-	if(Input.is_action_pressed("move_up"))
+	if(Input.is_action_pressed("move_up")):
+		motion.y -= 1
+	if(Input.is_action_pressed("move_down")):
+		motion.y += 1
+	if(Input.is_action_pressed("move_left")):
+		motion.x -= 1
+	if(Input.is_action_pressed("move_right")):
+		motion.x += 1
+	
+	motion = motion.normalized()
+	motion = move_and_slide(motion * movespeed)
+	
+	look_at(get_global_mouse_position())
 	pass
