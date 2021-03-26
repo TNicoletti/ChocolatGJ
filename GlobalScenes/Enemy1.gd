31775641aac0627356c67c1a0001dca8507a6978
TNicoletti <30930753+TNicoletti@users.noreply.github.com>
2021-03-25 23:01:25 -0300

    Inimigos detectam o player
    Inimigos agora detectam o player, o jogo reinicia se for detectado por muito tempo; inicio de movimento dos inimigos

--- GlobalScenes/Enemy1.gd
+++ GlobalScenes/Enemy1.gd
@@ -2,16 +2,48 @@ extends KinematicBody2D
 
 var patent = 1
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var detection_level = 0
 
+const DETECTION_INCREASE_FACTOR = 0.1
+const DETECTION_DECREASE_FACTOR = 0.05
+var detected: bool = false
 
-# Called when the node enters the scene tree for the first time.
+onready var player_patent = get_node("../Player/Sprite")
+
+onready var lbl_detection = get_node("Dtl")
+
 func _ready():
+	get_node("Patent").refresh(patent)
+	pass
+	
+onready var movement_enemy = get_node("Movement_enemy")
+
+func _process(delta):
+	
+	detection_handle(delta)
+	
+	
+func detection_handle(_delta):
+	if(detected && player_patent.patent < patent):
+		detection_level += DETECTION_INCREASE_FACTOR
+	else:
+		detection_level -= DETECTION_DECREASE_FACTOR
+	detection_level = clamp(detection_level, 0, 100)
+	
+	if(detection_level >= 50):
+		#movement_enemy.state = 1
+		get_tree().reload_current_scene()
+	
+	#Test
+	lbl_detection.text = str("Detection: " + str("%0.2f" % detection_level))
+
+func _on_Sight_body_entered(body):
+	if("Player" in body.name):
+		detected = true
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Sight_body_exited(body):
+	if("Player" in body.name):
+		detected = false
+	pass # Replace with function body.
